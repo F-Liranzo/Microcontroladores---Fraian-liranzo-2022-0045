fe6cd38caffb9728f2ad621738501840c9417e73
F-Liranzo <liranzo.fraian@gmail.com>
2024-12-02 18:23:05 -0400

    Maquina de estados local
    aqui se le asignaron los pines al esp32

--- Microcontroladores/Puerta_1/main/main.c
+++ Microcontroladores/Puerta_1/main/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include<stdlib.h>
 
+// librerias esp-32
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
 #define ESTADO_INIT 0
 #define ESTADO_CERRADO 1
 #define ESTADO_ABIERTO 2
@@ -13,8 +18,21 @@
 #define ERROR_LS 1
 #define ERROR_RT 2
 
+
+// Asignarle pines de salida al esp 32 // 
+#define limit_shwitch_abierto 26
+#define limit_shwitch_cerrado 5
+#define MOTOR_ABRIR 2
+#define MOTOR_CERRAR 4
+#define BOTON 34
+#define LED_ABRIENDO
+#define LED_CERRANDO 
+#define LED_ERROR 15
+
+
+
 int ESTADO_SIGUIENTE    = ESTADO_INIT;
-int ESTADO_ACTUAL       = ESTADO_INIT ;
+int ESTADO_ACTUAL       = ESTADO_INIT;
 int ESTADO_ANTERIOR     = ESTADO_INIT; 
 
 struct DATA_IO  //ESTRUCTURA DE ENTRADA DE DATOS , en este caso los sensores 
@@ -36,6 +54,54 @@ struct DATA_IO  //ESTRUCTURA DE ENTRADA DE DATOS , en este caso los sensores
 
 
 } data_io;
+
+void cofiguracion_salidas (void)
+{
+// Configurar LED como salida
+    gpio_reset_pin(LED_ABRIENDO);
+    gpio_set_direction(LED_ABRIENDO, GPIO_MODE_OUTPUT);
+
+     gpio_reset_pin(LED_CERRANDO);
+    gpio_set_direction(LED_CERRANDO, GPIO_MODE_OUTPUT);
+
+    gpio_reset_pin(MOTOR_ABRIR);
+    gpio_set_direction(MOTOR_ABRIR, GPIO_MODE_OUTPUT);
+
+    gpio_reset_pin(MOTOR_CERRAR);
+    gpio_set_direction(MOTOR_CERRAR, GPIO_MODE_OUTPUT);
+
+    gpio_reset_pin(LED_ERROR);
+    gpio_set_direction(LED_ERROR, GPIO_MODE_OUTPUT);
+
+
+    // Configurar botón como entrada con pull-down
+    gpio_reset_pin(BOTON);
+    gpio_set_pull_mode(BOTON, GPIO_PULLDOWN_ONLY);
+    gpio_set_direction(BOTON, GPIO_MODE_INPUT);
+
+     gpio_reset_pin(limit_shwitch_abierto);
+    gpio_set_pull_mode(limit_shwitch_abierto, GPIO_PULLDOWN_ONLY);
+    gpio_set_direction(limit_shwitch_abierto, GPIO_MODE_INPUT);
+
+     gpio_reset_pin(limit_shwitch_cerrado);
+    gpio_set_pull_mode(limit_shwitch_cerrado, GPIO_PULLDOWN_ONLY);
+    gpio_set_direction(limit_shwitch_cerrado, GPIO_MODE_INPUT);
+}
+
+void actualizar_datos (void)
+{
+    //data_io.DATOS_READY = FALSE;
+    vTaskDelay(10/portTICK_PERIOD_MS);
+
+    data_io.LSA = gpio_get_level(limit_shwitch_abierto);
+    data_io.LSC = gpio_get_level(limit_shwitch_cerrado);
+    gpio_set_level(MOTOR_ABRIR, data_io.MA);
+    gpio_set_level(MOTOR_CERRAR, data_io.MC);
+    gpio_set_level(LED_ABRIENDO, data_io.Led_A);
+    gpio_set_level(LED_CERRANDO, data_io.Led_C);
+    gpio_set_level(LED_ERROR, data_io.led_ERR);
+    //data_io.DATOS_READY = TRUE;
+}
 
 
 int main ()
@@ -76,6 +142,8 @@ int main ()
 
 int Func_INIT(void)
 {
+
+   
    ESTADO_ANTERIOR = ESTADO_INIT ;
    ESTADO_ACTUAL = ESTADO_INIT ;
    data_io.MC = FALSE;
@@ -85,12 +153,12 @@ int Func_INIT(void)
    data_io.led_C = TRUE;
    data_io.led_ERR = TRUE;
 
-   delay();
-    data_io.led_A = FALSE;
-   data_io.led_C = FALSE;
-   data_io.led_ERR = FALSE;
+   //delay();
+    //data_io.led_A = FALSE;
+   //data_io.led_C = FALSE;
+   //data_io.led_ERR = FALSE;
  // data_io.COD_ERR = FALSE;
-  data_io.cont_RT = 0;
+  //data_io.cont_RT = 0;
  // data_io.DATOS_READY = FALSE;
 
  // while (!data_io.DATOS_READY); // SI LOS DATOS NO SON LEIDOS EL PROGRAMA NO CORRE 
@@ -135,7 +203,9 @@ int Func_CERRADO(void)
    data_io.led_A= FALSE;
    data_io.led_C = FALSE;
    data_io.led_ERR = FALSE;
+   printf("\n Cerrado\n");
 
+   actualizar_datos ();
 
    for (;;)
    {
@@ -159,6 +229,10 @@ int Func_ABIERTO(void)
    data_io.led_C = FALSE;
    data_io.led_ERR = FALSE;
 
+    printf("\n abierto\n");
+
+   actualizar_datos ();
+
 
    for (;;)
    {
@@ -180,6 +254,10 @@ int Func_CERRANDO(void)
     data_io.led_A= FALSE;
    data_io.led_C = TRUE;
    data_io.led_ERR = FALSE;
+
+    printf("\n cerrando \n");
+
+   actualizar_datos ();
 
    for (;;)
    {
@@ -208,6 +286,10 @@ int Func_ABRIENDO(void)
    data_io.led_C = FALSE;
    data_io.led_ERR = FALSE;
 
+    printf("\n abriendo\n");
+
+   actualizar_datos ();
+
    for (;;)
    {
       // Si el limit switch abierto es verdadero , devuelve el estado abierto 
@@ -224,7 +306,11 @@ int Func_ABRIENDO(void)
    }
    
 }
-int Func_ERROR(void)
+int Func_ERROR(void)                                   
+
+ printf("\n error \n");
+
+   actualizar_datos ();
 
 { 
    ESTADO_ANTERIOR = ESTADO_ACTUAL;
